d35a2222167e37237e2f747c849141015cc435c2
elliot crowder <elliot.k.crowder@gmail.com>
2024-06-18 21:15:33 -0700

    getDrops method added but not implemented

--- src/main/java/net/elliot/blackpanthermod/item/RawVibranium.java
+++ src/main/java/net/elliot/blackpanthermod/item/RawVibranium.java
@@ -1,4 +1,10 @@
 package net.elliot.blackpanthermod.item;
 
+import net.minecraft.world.item.ItemStack;
+
 public class RawVibranium {
+
+    public List<ItemStack> getDrops(){
+        return null;
+    }
 }

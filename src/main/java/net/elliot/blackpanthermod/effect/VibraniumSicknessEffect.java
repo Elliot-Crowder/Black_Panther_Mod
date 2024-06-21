f4051b40f87be5171a1b3b738315f4fb601cb685
elliot crowder <elliot.k.crowder@gmail.com>
2024-06-21 15:23:04 -0700

    vibraniumSicknessEffect now causes exhaustian no matter how many ticks have elapsed while the effect has been active. Damage can be modified to only be applied to the player after a set duration of ticks have elapsed.

--- src/main/java/net/elliot/blackpanthermod/effect/VibraniumSicknessEffect.java
+++ src/main/java/net/elliot/blackpanthermod/effect/VibraniumSicknessEffect.java
@@ -7,30 +7,48 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class VibraniumSicknessEffect extends MobEffect {
+    private int numTicksElapsed;
+
     protected VibraniumSicknessEffect(MobEffectCategory pCategory, int pColor) {
         super(pCategory, pColor);
+        this.numTicksElapsed = 0;
     }
+
+//    @SubscribeEvent
+//    public void playerTickEvent(TickEvent.PlayerTickEvent event){
+//
+//    }
+
 
     @Override
     public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier){
         MobEffectInstance effectInstance = pLivingEntity.getEffect(this);
         if(effectInstance !=  null){
             int duration = effectInstance.getDuration();
-            if(duration>0){
-                if(duration>200){
+            this.numTicksElapsed++;
+            if(duration==1) {
+                resetNumTicksElapsed();
+            }else if(duration>0){
+                if(pLivingEntity instanceof Player player){
+                    player.causeFoodExhaustion(0.1f);
+                }
+                if(this.numTicksElapsed<200){
+                    //be sussy
+                }else{
+                    //hurt the player if more than 400 ticks have elapsed
                     //pLivingEntity.hurt(,1.0f);
                     ;
-                }else{
-                    if(pLivingEntity instanceof Player){
-                       Player player = (Player) pLivingEntity;
-                       player.causeFoodExhaustion(1.0f);
-                    }
-
                 }
             }
         }
+    }
+
+    private void resetNumTicksElapsed(){
+        this.numTicksElapsed = 0;
     }
 
     @Override

4ef8514891a9f6691bbd7413bc1d1ad77eed1611
Malek Owaidat <owaidatmalek8@gmail.com>
2024-06-24 23:49:18 -0700

    updated requirements.txt

--- src/main/java/net/elliot/blackpanthermod/effect/VibraniumSicknessEffect.java
+++ src/main/java/net/elliot/blackpanthermod/effect/VibraniumSicknessEffect.java
@@ -7,6 +7,7 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.player.Player;
 import java.lang.reflect.Method;
 import net.minecraft.world.level.Level;

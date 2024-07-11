559c4baa0c9aaff4c33b9ee441c60eb7a16061d8
elliot crowder <elliot.k.crowder@gmail.com>
2024-07-10 18:19:21 -0700

    Block Entity implementation for RawVibraniumBlock. Need to add ticking functionality.

--- src/main/java/net/elliot/blackpanthermod/block/RawVibraniumBlock.java
+++ src/main/java/net/elliot/blackpanthermod/block/RawVibraniumBlock.java
@@ -1,6 +1,9 @@
 package net.elliot.blackpanthermod.block;
 
+import net.elliot.blackpanthermod.blockentity.RawVibraniumBlockEntity;
+import net.elliot.blackpanthermod.blockentity.util.TickableBlockEntity;
 import net.elliot.blackpanthermod.effect.ModEffects;
+import net.elliot.blackpanthermod.init.BlockEntityInit;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -9,10 +12,13 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import java.util.List;
 import java.util.Random;
 import net.minecraft.world.phys.AABB;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class RawVibraniumBlock extends Block implements EntityBlock {
@@ -22,9 +28,16 @@ public class RawVibraniumBlock extends Block implements EntityBlock {
                 .lightLevel((p_152607_) -> { return 7; }));
     }
 
-    @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return null;
+       return BlockEntityInit.RAW_VIBRANIUM_BLOCK_ENTITY.get().create(blockPos,blockState);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type){
+        return TickableBlockEntity.getTickerHelper(level);
     }
 }
+
+
--- src/main/java/net/elliot/blackpanthermod/blockentity/RawVibraniumBlockEntity.java
+++ src/main/java/net/elliot/blackpanthermod/blockentity/RawVibraniumBlockEntity.java
@@ -1,14 +1,24 @@
 package net.elliot.blackpanthermod.blockentity;
 
+import net.elliot.blackpanthermod.blockentity.util.TickableBlockEntity;
 import net.elliot.blackpanthermod.init.BlockEntityInit;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
-public class RawVibraniumBlockEntity extends BlockEntity {
+import javax.annotation.Nullable;
+
+public class RawVibraniumBlockEntity extends BlockEntity implements TickableBlockEntity {
 
     public RawVibraniumBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(BlockEntityInit.RAW_VIBRANIUM_BLOCK_ENTITY.get(), pPos, pBlockState);
+    }
+
+
+    @Override
+    public void tick() {
+        
     }
 }

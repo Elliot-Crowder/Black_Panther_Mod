444f2a4b1f2e8354feff64a53a348d945c0ba805
Malek Owaidat <owaidatmalek8@gmail.com>
2024-07-20 23:14:07 -0700

    added XP drops

--- src/main/java/net/elliot/blackpanthermod/block/RawVibraniumBlock.java
+++ src/main/java/net/elliot/blackpanthermod/block/RawVibraniumBlock.java
@@ -1,13 +1,9 @@
 package net.elliot.blackpanthermod.block;
 
-import net.elliot.blackpanthermod.blockentity.RawVibraniumBlockEntity;
 import net.elliot.blackpanthermod.blockentity.util.TickableBlockEntity;
-import net.elliot.blackpanthermod.effect.ModEffects;
 import net.elliot.blackpanthermod.init.BlockEntityInit;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
@@ -15,11 +11,10 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import java.util.List;
-import java.util.Random;
-import net.minecraft.world.phys.AABB;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
 
 public class RawVibraniumBlock extends Block implements EntityBlock {
     public RawVibraniumBlock(Properties pProperties) {
@@ -31,6 +26,12 @@ public class RawVibraniumBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
        return BlockEntityInit.RAW_VIBRANIUM_BLOCK_ENTITY.get().create(blockPos,blockState);
+    }
+
+    @Override
+    public int getExpDrop(BlockState state, LevelReader level, RandomSource randomSource, BlockPos pos, int fortuneLevel, int silkTouchLevel) {
+        Random rand = new Random();
+        return silkTouchLevel == 0 ? rand.nextInt(6) + 3 : 0;
     }
 
     @Nullable

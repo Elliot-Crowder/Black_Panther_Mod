38812efacd34dee848eef76844b545e17bcef77d
Malek Owaidat <owaidatmalek8@gmail.com>
2024-07-23 01:49:58 -0700

    optimized for performance

--- src/main/java/net/elliot/blackpanthermod/blockentity/RawVibraniumBlockEntity.java
+++ src/main/java/net/elliot/blackpanthermod/blockentity/RawVibraniumBlockEntity.java
@@ -14,32 +14,27 @@ import java.util.List;
 
 public class RawVibraniumBlockEntity extends BlockEntity implements TickableBlockEntity {
 
-    private final BlockPos blockPos;
+    private int tickCounter;
 
     public RawVibraniumBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.RAW_VIBRANIUM_BLOCK_ENTITY.get(), pPos, pBlockState);
-        this.blockPos = pPos;
     }
 
     @Override
     public void tick() {
+        tickCounter++;
         Level pLevel = this.getLevel();
-        AABB effectArea = new AABB(this.blockPos).inflate(3);
-        List<Player> players = pLevel.getEntitiesOfClass(Player.class, effectArea);
-        for (Player player : pLevel.players()) {
-            MobEffectInstance existingSicknessEffect = player.getEffect(ModEffects.VIBRANIUM_SICKNESS.get());
-            MobEffectInstance existingDecayEffect = player.getEffect(ModEffects.VIBRANIUM_DECAY.get());
-            if (effectArea.contains(player.getBoundingBox().getCenter())) {
+        AABB effectArea = new AABB(this.worldPosition).inflate(7);
+        List<Player> playersInArea = pLevel.getEntitiesOfClass(Player.class, effectArea);
+        if (tickCounter % 20 == 0) {
+            tickCounter = 0;
+            for (Player player : playersInArea) {
+                MobEffectInstance existingSicknessEffect = player.getEffect(ModEffects.VIBRANIUM_SICKNESS.get());
+                MobEffectInstance existingDecayEffect = player.getEffect(ModEffects.VIBRANIUM_DECAY.get());
                 if (existingSicknessEffect == null && existingDecayEffect == null) {
                     player.addEffect(new MobEffectInstance(ModEffects.VIBRANIUM_SICKNESS.get(), 600));
-                } else if (existingSicknessEffect != null && existingSicknessEffect.getDuration() < 20) {
-                    if (Math.random() * 100 < 33.33) {
-                        player.addEffect(new MobEffectInstance(ModEffects.VIBRANIUM_DECAY.get(), -1));
-                    }
-                }
-            } else {
-                if (existingSicknessEffect != null && existingSicknessEffect.getDuration() < 20) {
-                    player.removeEffect(ModEffects.VIBRANIUM_SICKNESS.get());
+                } else if (existingSicknessEffect != null && existingSicknessEffect.getDuration() <= 20) {
+                    player.addEffect(new MobEffectInstance(ModEffects.VIBRANIUM_DECAY.get(), -1));
                 }
             }
         }

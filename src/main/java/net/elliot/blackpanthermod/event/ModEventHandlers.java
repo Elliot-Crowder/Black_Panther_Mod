5f4992ea3de54edd18fee4a63ea0a013bcfadaf2
Malek Owaidat <owaidatmalek8@gmail.com>
2024-07-24 14:57:20 -0700

    initializes the new commands

--- src/main/java/net/elliot/blackpanthermod/event/ModEventHandlers.java
+++ src/main/java/net/elliot/blackpanthermod/event/ModEventHandlers.java
@@ -1,28 +1,32 @@
 package net.elliot.blackpanthermod.event;
 
-
-import net.elliot.blackpanthermod.BlackPantherMod;
+import net.elliot.blackpanthermod.command.AddPantherPower;
+import net.elliot.blackpanthermod.command.CheckPantherPower;
+import net.elliot.blackpanthermod.command.RemovePantherPower;
 import net.elliot.blackpanthermod.playercap.BlackPantherPower;
 import net.elliot.blackpanthermod.playercap.BlackPantherPowerCapability;
-import net.elliot.blackpanthermod.playercap.util.PantherPower;
 import net.elliot.blackpanthermod.playercap.util.PantherPowerProvider;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
+import static net.elliot.blackpanthermod.BlackPantherMod.MOD_ID;
 
-
-@Mod.EventBusSubscriber(modid="blackpanthermod",bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ModEventHandlers {
 
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent event){
+        AddPantherPower.register(event.getDispatcher());
+        CheckPantherPower.register(event.getDispatcher());
+        RemovePantherPower.register(event.getDispatcher());
+    }
 
     @SubscribeEvent
     public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
@@ -30,24 +34,23 @@ public class ModEventHandlers {
     }
 
     @SubscribeEvent
-    public static void onAttachCapabilitesPlayer(AttachCapabilitiesEvent<Entity> event){
-        if(event.getObject() instanceof Player){
-            if(!event.getObject().getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).isPresent()){
-                event.addCapability(new ResourceLocation(BlackPantherMod.MOD_ID,"properties"), new PantherPowerProvider());
+    public static void onAttachCapabilitesPlayer(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof Player) {
+            if (!event.getObject().getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).isPresent()) {
+                event.addCapability(new ResourceLocation(MOD_ID,"properties"), new PantherPowerProvider());
             }
         }
     }
 
     @SubscribeEvent
     public void onClonePlayer(PlayerEvent.Clone e) {
-        if(e.isWasDeath()){
+        if (e.isWasDeath()) {
             // Retrieve the capability from the original player
             e.getOriginal().getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(oldStore -> {
                 e.getOriginal().getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(newStore -> {
                     newStore.copyFrom(oldStore);
                 });
             });
-
         }
     }
 }

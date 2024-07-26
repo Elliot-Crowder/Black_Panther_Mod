8fe483f6e130eeee17ecf32516d9f361e1b7c6f0
Malek Owaidat <owaidatmalek8@gmail.com>
2024-07-26 11:55:58 -0700

    modified values for balancing

--- src/main/java/net/elliot/blackpanthermod/playercap/BlackPantherPowerCapability.java
+++ src/main/java/net/elliot/blackpanthermod/playercap/BlackPantherPowerCapability.java
@@ -16,7 +16,7 @@ public class BlackPantherPowerCapability {
         });
         pTarget.getAttribute(Attributes.MAX_HEALTH).setBaseValue(40.0);
         pTarget.getAttribute(Attributes.KNOCKBACK_RESISTANCE).setBaseValue(1.0);
-        pTarget.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.5);
+        pTarget.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.15);
         pTarget.getAttribute(Attributes.ATTACK_DAMAGE).setBaseValue(6.0);
         pTarget.getAttribute(Attributes.ATTACK_KNOCKBACK).setBaseValue(2.0);
         pTarget.getAttribute(Attributes.ATTACK_SPEED).setBaseValue(8.0);

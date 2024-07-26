00972acbe58bab5704eb78906dc31f8d23199654
Malek Owaidat <owaidatmalek8@gmail.com>
2024-07-26 11:56:38 -0700

    added subscribe event for player jumping

--- src/main/java/net/elliot/blackpanthermod/event/PlayerCapEvents.java
+++ src/main/java/net/elliot/blackpanthermod/event/PlayerCapEvents.java
@@ -1,21 +1,22 @@
 package net.elliot.blackpanthermod.event;
 
+import net.elliot.blackpanthermod.BlackPantherMod;
 import net.elliot.blackpanthermod.playercap.BlackPantherPower;
 import net.elliot.blackpanthermod.playercap.BlackPantherPowerCapability;
 import net.elliot.blackpanthermod.playercap.util.PantherPowerProvider;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import static net.elliot.blackpanthermod.BlackPantherMod.MOD_ID;
-
-@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = BlackPantherMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class PlayerCapEvents {
 
     @SubscribeEvent
@@ -25,10 +26,19 @@ public class PlayerCapEvents {
 
     @SubscribeEvent
     public static void onAttachCapabilitesPlayer(AttachCapabilitiesEvent<Entity> event) {
-        if (event.getObject() instanceof ServerPlayer) {
-            if (!event.getObject().getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).isPresent()) {
-                event.addCapability(new ResourceLocation(MOD_ID,"properties"), new PantherPowerProvider());
-            }
+        if (event.getObject() instanceof ServerPlayer && !event.getObject().getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).isPresent()) {
+            event.addCapability(new ResourceLocation(BlackPantherMod.MOD_ID,"properties"), new PantherPowerProvider());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerJump(LivingEvent.LivingJumpEvent event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            player.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(power -> {
+                if (power.hasPower()) {
+                    player.
+                }
+            });
         }
     }
 
@@ -36,11 +46,10 @@ public class PlayerCapEvents {
     public static void onClonePlayer(PlayerEvent.Clone e) {
         if (e.getEntity() instanceof ServerPlayer playerNew && e.getOriginal() instanceof ServerPlayer playerOld) {
             playerOld.reviveCaps();
-            playerNew.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(capabilityNew ->
-                    playerOld.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(capabilityOld ->
-                            capabilityNew.deserializeNBT(capabilityOld.serializeNBT())));
-            playerOld.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(power -> {
-                if (power.hasPower()) {
+            playerNew.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(capabilityNew -> {
+                playerOld.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(capabilityOld ->
+                        capabilityNew.deserializeNBT(capabilityOld.serializeNBT()));
+                if (capabilityNew.hasPower()) {
                     BlackPantherPowerCapability.modifyPlayerAttributes(playerNew);
                     if (e.isWasDeath()) {
                         playerNew.setHealth(40.0F);

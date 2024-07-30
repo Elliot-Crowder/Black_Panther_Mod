5ede37c95a2f31b34d52e57d12b6cafd69eed27a
Malek Owaidat <owaidatmalek8@gmail.com>
2024-07-30 06:44:01 -0700

    global chat messages when player gets panther or loses panther power

--- src/main/java/net/elliot/blackpanthermod/item/BlackPantherFruit.java
+++ src/main/java/net/elliot/blackpanthermod/item/BlackPantherFruit.java
@@ -1,16 +1,16 @@
 package net.elliot.blackpanthermod.item;
 
 import net.elliot.blackpanthermod.playercap.BlackPantherPowerCapability;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 
 public class BlackPantherFruit extends Item {
     public BlackPantherFruit(Properties pProperties) {
@@ -28,10 +28,15 @@ public class BlackPantherFruit extends Item {
             .build();
 
     @Override
-    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level world, @NotNull LivingEntity player) {
-        if (!world.isClientSide && player instanceof ServerPlayer pTarget) {
-            BlackPantherPowerCapability.modifyPlayerAttributes(pTarget);
-        }
+    public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity player) {
+        player.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(power -> {
+            if (!power.hasPower()) {
+                BlackPantherPowerCapability.modifyPlayerAttributes(player);
+                for (Player serverPlayer : world.getServer().getPlayerList().getPlayers()) {
+                    serverPlayer.sendSystemMessage(Component.literal(player.getName().getString() + " has gained the power of The Black Panther"));
+                }
+            }
+        });
         return super.finishUsingItem(stack, world, player);
     }
 }
--- src/main/java/net/elliot/blackpanthermod/item/DormantBlackPantherFruit.java
+++ src/main/java/net/elliot/blackpanthermod/item/DormantBlackPantherFruit.java
@@ -1,24 +1,26 @@
 package net.elliot.blackpanthermod.item;
 
 import net.elliot.blackpanthermod.playercap.BlackPantherPowerCapability;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 
-public class DormantBlackPantherFruit extends Item  {
+public class DormantBlackPantherFruit extends Item {
     public DormantBlackPantherFruit(Properties pProperties) {
         super(pProperties.rarity(Rarity.RARE).food(DormantBlackPantherFruit.DORMANTFRUIT));
     }
 
     public static final FoodProperties DORMANTFRUIT = new FoodProperties.Builder()
             .alwaysEat()
+            .saturationMod(2)
+            .nutrition(20)
             .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200), 1)
             .effect(() -> new MobEffectInstance(MobEffects.BLINDNESS, 200), 1)
             .effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 200), 1)
@@ -26,10 +28,15 @@ public class DormantBlackPantherFruit extends Item  {
             .build();
 
     @Override
-    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level world, @NotNull LivingEntity player) {
-        if (!world.isClientSide && player instanceof ServerPlayer pTarget) {
-            BlackPantherPowerCapability.resetPlayerAttributes(pTarget);
-        }
+    public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity player) {
+        player.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(power -> {
+            if (power.hasPower()) {
+                BlackPantherPowerCapability.resetPlayerAttributes(player);
+                for (Player serverPlayer : world.getServer().getPlayerList().getPlayers()) {
+                    serverPlayer.sendSystemMessage(Component.literal(player.getName().getString() + "'s connection to The Black Panther has faded"));
+                }
+            }
+        });
         return super.finishUsingItem(stack, world, player);
     }
 }

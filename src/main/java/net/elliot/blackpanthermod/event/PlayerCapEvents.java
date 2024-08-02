c34ee1de0e3b1e90d66d4ac9525e94a7aa44e4dc
elliot crowder <elliot.k.crowder@gmail.com>
2024-08-01 17:24:34 -0700

    nmo

--- src/main/java/net/elliot/blackpanthermod/event/PlayerCapEvents.java
+++ src/main/java/net/elliot/blackpanthermod/event/PlayerCapEvents.java
@@ -7,13 +7,17 @@ import net.elliot.blackpanthermod.playercap.util.PantherPowerProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -21,8 +25,11 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.UUID;
+
 @Mod.EventBusSubscriber(modid = BlackPantherMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class PlayerCapEvents {
+
 
     @SubscribeEvent
     public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
@@ -38,13 +45,30 @@ public class PlayerCapEvents {
 
     @SubscribeEvent
     public static void onPlayerJump(LivingEvent.LivingJumpEvent event) {
-        if (event.getEntity() instanceof ServerPlayer player) {
-            player.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(power -> {
-                if (power.hasPower()) {
-                    player.sendSystemMessage(Component.literal("attempting jump"));
-                }
-            });
+        float jumpMultiplier = 2F;
+
+        if (event.getEntity() instanceof ServerPlayer player){
+            player.sendSystemMessage(Component.literal("attempting jump"));
+            event.getEntity().push(0,jumpMultiplier,0);
         }
+
+//        if (event.getEntity() instanceof ServerPlayer player) {
+//            player.getCapability(BlackPantherPowerCapability.BLACK_PANTHER_POWER_CAPABILITY).ifPresent(power -> {
+//                if (power.hasPower()) {
+//                    player.sendSystemMessage(Component.literal("attempting jump"));
+//                    Vec3 vec3 = player.getDeltaMovement();
+//                    player.setDeltaMovement(vec3.x, (double)10, vec3.z);
+//                    if (player.isSprinting()) {
+//                        float f = player.getYRot() * 0.017453292F;
+//                        player.setDeltaMovement(player.getDeltaMovement().add((double)(-Mth.sin(f) * 0.2F), 0.0, (double)(Mth.cos(f) * 0.2F)));
+//                    }
+//
+//                    player.hasImpulse = true;
+//
+//
+//                }
+//            });
+//        }
     }
 
     @SubscribeEvent
